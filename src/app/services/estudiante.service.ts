6eb410aa1e6240bccd44ed4bb6657f7764cb3bc1
unknown <vserafin5@gmail.com>
2025-07-17 10:58:11 -0600

    Cambios pertinentes para el proyecto prueba de entrevista de trabajo

--- src/app/services/estudiante.service.ts
+++ src/app/services/estudiante.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 import { Estudiante } from '../models/student.model';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,17 @@ export class EstudianteService {
 
   private students: Estudiante[] = [];
   private studentsSubject = new BehaviorSubject<Estudiante[]>([]);
+  private generalesUrl = `${environment.API_URL}/general/`;
+
+  constructor( private http: HttpClient ) {}
+
+  generalesGet(ruta: any): Observable<any> {
+    return this.http.get(this.generalesUrl + ruta);
+  }
+
+  generalesPost(ruta: any, data: any): Observable<any> {
+    return this.http.post(this.generalesUrl + ruta, data);
+  }
 
   getStudents(): Observable<Estudiante[]> {
     return this.studentsSubject.asObservable();
